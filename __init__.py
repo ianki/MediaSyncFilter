5cd8848f3de8518cd0865730d9415a5ae10f33a2
ianki <iankigit@gmail.com>
2020-11-11 21:15:15 -0800

    Change filtered media path to ./filtered_media

--- __init__.py
+++ __init__.py
@@ -26,7 +26,7 @@ def my_redirect_path(self, path, _old):
 def media_file_filter(file_path):
   full_path = os.path.join(mw.pm.profileFolder(), file_path)
   if not os.path.exists(full_path):
-    alt_path = os.path.join('hidden', file_path)
+    alt_path = os.path.join('filtered_media', file_path)
     full_path = os.path.join(mw.pm.profileFolder(), alt_path)
     if os.path.exists(alt_path):
         file_path = alt_path
